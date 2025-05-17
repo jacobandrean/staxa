511f0b705ddea7b205433788a00c0ff4c70c7015
Jacob Andrean <jacob.a@avowsit.com>
2025-05-18 03:21:19 +0700

    enhance ListView

--- Sources/Staxa/Views/ListView.swift
+++ Sources/Staxa/Views/ListView.swift
@@ -8,25 +8,39 @@
 import Combine
 import UIKit
 
-public class ListView<P: Publisher, Item: Hashable>: StaxaView where P.Output == [Item], P.Failure == Never {
+public class ListView<Item: Hashable>: StaxaView {
     private let spacing: CGFloat
     private let identifier: String
-    private let data: P
     private let rowContent: (Item) -> UIView
-
-    public init(
+    @Published private var data: [Item] = []
+    
+    public init<P: Publisher>(
         _ data: P,
         spacing: CGFloat = 0,
-        identifier: String = UUID().uuidString,
+        identifier: String = "\(String(describing: UIViewHostingCollectionViewCell.self))_\(String(describing: Item.self))",
         rowContent: @escaping (Item) -> UIView
-    ) {
-        self.identifier = identifier
+    ) where P.Output == [Item], P.Failure == Never {
         self.spacing = spacing
-        self.data = data
+        self.identifier = identifier
         self.rowContent = rowContent
         super.init(frame: .zero)
+        data.weakAssign(to: \.data, on: self).store(in: &viewCancellables)
     }
-
+    
+    public convenience init(
+        _ data: [Item],
+        spacing: CGFloat = 0,
+        identifier: String = "\(String(describing: UIViewHostingCollectionViewCell.self))_\(String(describing: Item.self))",
+        rowContent: @escaping (Item) -> UIView
+    ) {
+        self.init(
+            Just(data).eraseToAnyPublisher(),
+            spacing: spacing,
+            identifier: identifier,
+            rowContent: rowContent
+        )
+    }
+    
     @MainActor required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -46,7 +60,7 @@ public class ListView<P: Publisher, Item: Hashable>: StaxaView where P.Output ==
         return UICollectionView(frame: .zero, collectionViewLayout: layout)
             .backgroundColor(.clear)
             .register(UIViewHostingCollectionViewCell.self, identifier: identifier)
-            .bind(to: data, section: 0) { collectionView, indexPath, model in
+            .bind(to: $data, section: 0) { collectionView, indexPath, model in
                 guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.identifier, for: indexPath) as? UIViewHostingCollectionViewCell else {
                     return UICollectionViewCell()
                 }
